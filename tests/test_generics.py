5c27069239bc465c4206ca33dcbc49fb1ae50b90
mihsan <mihsan395@gmail.com>
2025-01-21 13:42:02 +0300

    обновил зависимости

--- tests/test_generics.py
+++ tests/test_generics.py
@@ -1,20 +1,20 @@
-from typing import Generic, TypeVar, List, Union, Type, Any, Dict
+from typing import Any, Dict, Generic, List, Type, TypeVar
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from sqlmodel import SQLModel
 
-from class_based_fastapi.decorators import post, get
+from class_based_fastapi.decorators import get, post
 from class_based_fastapi.routable import Routable
 from tests.utilities import check_api_methods
 
 # region MODELS
 
-TReq = TypeVar('TReq')
-TRes = TypeVar('TRes')
-TReq1 = TypeVar('TReq1')
-TRes1 = TypeVar('TRes1')
+TReq = TypeVar("TReq")
+TRes = TypeVar("TRes")
+TReq1 = TypeVar("TReq1")
+TRes1 = TypeVar("TRes1")
 
 
 class BaseRequestBody(SQLModel):
@@ -42,82 +42,92 @@ class CloneBaseResponse(BaseResponse):
 class BaseView_TestGeneric(Routable, Generic[TReq, TRes]):
     _injected = 1
 
-    @post(path='add')
+    @post(path="add")
     def add(self, x: TReq) -> TRes:
-        return BaseResponse(param1=999, param2='test')
+        return BaseResponse(param1=999, param2="test")
 
-    @get(path='')
+    @get(path="")
     def list(self) -> List[TRes]:
         return list()
 
 
-class ExampleRoutableParent_TestGeneric(BaseView_TestGeneric[BaseRequestBody, BaseResponse]):
-    NAME_MODULE = 'Test'
+class ExampleRoutableParent_TestGeneric(
+    BaseView_TestGeneric[BaseRequestBody, BaseResponse]
+):
+    NAME_MODULE = "Test"
 
 
-class BaseViewParent_TestGeneric(BaseView_TestGeneric[TReq1, TRes1], Generic[TReq1, TRes1]):
+class BaseViewParent_TestGeneric(
+    BaseView_TestGeneric[TReq1, TRes1], Generic[TReq1, TRes1]
+):
     pass
 
 
-class ExampleRoutableParent1_TestGeneric(BaseViewParent_TestGeneric[CloneBaseRequestBody, CloneBaseResponse]):
+class ExampleRoutableParent1_TestGeneric(
+    BaseViewParent_TestGeneric[CloneBaseRequestBody, CloneBaseResponse]
+):
     pass
 
 
 # endregion
 # region Generic Types
 
-class TestClass(SQLModel):
+
+class MyTestClass(SQLModel):
     data: Any
 
 
-TReqGen1 = TypeVar('TReqGen1')
-TReqGen2 = TypeVar('TReqGen2')
-TResGen1 = TypeVar('TResGen1')
+TReqGen1 = TypeVar("TReqGen1")
+TReqGen2 = TypeVar("TReqGen2")
+TResGen1 = TypeVar("TResGen1")
 
-TReqGen3 = TypeVar('TReqGen3')
-TReqGen4 = TypeVar('TReqGen4')
-TResGen3 = TypeVar('TResGen3')
+TReqGen3 = TypeVar("TReqGen3")
+TReqGen4 = TypeVar("TReqGen4")
+TResGen3 = TypeVar("TResGen3")
 
 
 class BV_Generic(Routable, Generic[TReqGen1, TReqGen2, TResGen1]):
     _injected = 1
 
-    @post(path='convert')
+    @post(path="convert")
     def add(self, x: TReqGen1) -> List[TResGen1]:
         return [TRes(data=i) for i in x]
 
-    @post(path='test/{x}')
+    @post(path="test/{x}")
     def test(self, x: str) -> str:
         return [TRes(data=i) for i in x]
 
-    @get(path='get-list/{x}')
+    @get(path="get-list/{x}")
     def get_list(self, x: TReqGen2) -> List[TResGen1]:
         if isinstance(x, str):
             x = int(x)
         return [TRes(data=i) for i in range(x)]
 
 
-class BV_Middle_Generic1(BV_Generic[TReqGen3, TReqGen4, TResGen3], Generic[TReqGen3, TReqGen4, TResGen3]):
+class BV_Middle_Generic1(
+    BV_Generic[TReqGen3, TReqGen4, TResGen3], Generic[TReqGen3, TReqGen4, TResGen3]
+):
     pass
 
 
-class BVParent_Generic2(BV_Middle_Generic1[List[int], int, Dict[str, TestClass]]):
+class BVParent_Generic2(BV_Middle_Generic1[List[int], int, Dict[str, MyTestClass]]):
     pass
 
 
-class BVParent_Generic3(BV_Middle_Generic1[List[int], str, TestClass]):
+class BVParent_Generic3(BV_Middle_Generic1[List[int], str, MyTestClass]):
     pass
 
 
 # endregion
 # endregion
+
 
 ###################################################
 #################### TESTS ########################
 ###################################################
 class TrueRoute(SQLModel):
-    RequestType: Union[type, Any, None]
-    ResponseType: Union[type, Any, None]
+    RequestType: Any = None
+    ResponseType: Any = None
 
 
 def create_app(controllers: List[Type[Routable]]) -> FastAPI:
@@ -128,19 +138,22 @@ def create_app(controllers: List[Type[Routable]]) -> FastAPI:
     return app
 
 
-ORIGIN = '__origin__'
-ARGS = '__args__'
+ORIGIN = "__origin__"
+ARGS = "__args__"
 
 
 def is_equal_types(real_val, true_val) -> bool:
     if hasattr(real_val, ORIGIN) and hasattr(true_val, ORIGIN):
-        return getattr(real_val, ORIGIN) == getattr(true_val, ORIGIN) \
-               and getattr(real_val, ARGS) == getattr(true_val, ARGS)
+        return getattr(real_val, ORIGIN) == getattr(true_val, ORIGIN) and getattr(
+            real_val, ARGS
+        ) == getattr(true_val, ARGS)
 
-    return real_val is true_val \
-           or (real_val is None and true_val is None) \
-           or (true_val is str and real_val is None) \
-           or isinstance(real_val, true_val)
+    return (
+        real_val is true_val
+        or (real_val is None and true_val is None)
+        or (true_val is str and real_val is None)
+        or isinstance(real_val, true_val)
+    )
 
 
 def test_generic__generic_types() -> None:
@@ -151,9 +164,15 @@ def test_generic__generic_types() -> None:
         ]
     )
     true_route = {
-        '/bv-parent-generic2/v1.0/convert': TrueRoute(RequestType=List[int], ResponseType=List[Dict[str, TestClass]]),
-        '/bv-parent-generic3/v1.0/get-list/{x}': TrueRoute(RequestType=str, ResponseType=List[TestClass]),
-        '/bv-parent-generic3/v1.0/test/{x}': TrueRoute(RequestType=str, ResponseType=str),
+        "/bv-parent-generic2/v1.0/convert": TrueRoute(
+            RequestType=List[int], ResponseType=List[Dict[str, MyTestClass]]
+        ),
+        "/bv-parent-generic3/v1.0/get-list/{x}": TrueRoute(
+            RequestType=str, ResponseType=List[MyTestClass]
+        ),
+        "/bv-parent-generic3/v1.0/test/{x}": TrueRoute(
+            RequestType=str, ResponseType=str
+        ),
     }
     for path, type_ in true_route.items():
         app_routes: List[APIRoute] = list(filter(lambda x: x.path == path, app.routes))
@@ -162,7 +181,9 @@ def test_generic__generic_types() -> None:
         response_model = app_routes[0].response_model
         assert is_equal_types(response_model, type_.ResponseType)
 
-        request_type = None if app_routes[0].body_field is None else app_routes[0].body_field.annotation
+        request_type = (
+            None if app_routes[0].body_field is None else app_routes[0].body_field.type_
+        )
         assert is_equal_types(request_type, type_.RequestType)
 
 
@@ -176,14 +197,14 @@ def test_generic() -> None:
     )
 
     true_route = {
-        '/example-routable-parent1-test-generic/v1.0/add': TrueRoute(
+        "/example-routable-parent1-test-generic/v1.0/add": TrueRoute(
             RequestType=CloneBaseRequestBody, ResponseType=CloneBaseResponse
         ),
-        '/test/example-routable-parent-test-generic/v1.0/add': TrueRoute(
+        "/test/example-routable-parent-test-generic/v1.0/add": TrueRoute(
             RequestType=BaseRequestBody,
             ResponseType=BaseResponse,
         ),
-        '/base-view-test-generic/v1.0/add': TrueRoute(ResponseType=TypeVar),
+        "/base-view-test-generic/v1.0/add": TrueRoute(ResponseType=TypeVar),
     }
     for path, type_ in true_route.items():
         app_routes: List[APIRoute] = list(filter(lambda x: x.path == path, app.routes))
@@ -192,11 +213,13 @@ def test_generic() -> None:
         response_model = app_routes[0].response_model
         assert is_equal_types(response_model, type_.ResponseType)
 
-        request_type = None if app_routes[0].body_field is None else app_routes[0].body_field.annotation
+        request_type = (
+            None if app_routes[0].body_field is None else app_routes[0].body_field.type_
+        )
         assert is_equal_types(request_type, type_.RequestType)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # test_generic__generic_types()
 
     create = lambda: create_app(
