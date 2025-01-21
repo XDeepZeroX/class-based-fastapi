2f9986b9d3b1767aba0f6d2f35c605a3ec6f2ffa
XDeepZeroX <XDeepZeroX>
2025-01-21 10:36:19 +0300

    Fix requirements.txt

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def load_requirements(fname: str) -> list:
 
 setup(
     name='class-based-fastapi',
-    version='1.0.3',
+    version='1.0.4',
     author='XDeepZeroX',
     license='MIT',
     description='Class based routing for FastAPI',
